8bce911265054d0a919bf28c130b5b0271c1ec58
Stanley Mair <stanleygmair@gmail.com>
2022-05-11 05:17:19 +1200

    Completed basic functionality of the game

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
 
+const getAbsoluteDistance = (numberA, numberB) => {
+    return Math.abs(numberA - numberB);
+}
+
+const compareGuesses = (humanGuess, computerGuess, targetNumber) => {
+    let humanGuessDifference = getAbsoluteDistance(humanGuess, targetNumber);
+    let computerGuessDifference = getAbsoluteDistance(computerGuess, targetNumber);
+    if (humanGuessDifference === computerGuessDifference) {
+        return true;
+    } else if (humanGuessDifference < computerGuessDifference) {
+        return true;
+    } else if (computerGuessDifference < humanGuessDifference) {
+        return false;
+    }
+}
+
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber++;
+}
